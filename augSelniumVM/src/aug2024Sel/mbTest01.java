c16440c04c67d203f551ba620835ca271e8a360c
mbommena <mbommena@yahoo.com>
2024-08-04 12:48:47 +1000

    Pushing the code 1st time

---
+++ augSelniumVM/src/aug2024Sel/mbTest01.java
@@ -0,0 +1,14 @@
+package aug2024Sel;
+
+public class mbTest01 {
+
+		public void method1() 
+		{
+			System.out.println("Automation class method");
+			System.out.println("for Project1");
+		}
+		public static void main(String[] args) {
+			mbTest01 ob=new mbTest01();
+			ob.method1();
+		}
+	}
