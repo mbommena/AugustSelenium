7814b2b008abc776f9d8acde922d2d7b360c8f5e
mbommena <mbommena@yahoo.com>
2024-08-04 13:11:40 +1000

    Adding a second file to comit to AugustSelenium Repo

---
+++ augSelniumVM/src/aug2024Sel/aug2024Sel02.java
@@ -0,0 +1,14 @@
+package aug2024Sel;
+
+public class aug2024Sel02 {
+
+		public void method1() 
+		{
+			System.out.println("This is a second file to add");
+			System.out.println("Adding to AugustSelenium Repo");
+		}
+		public static void main(String[] args) {
+			aug2024Sel02 ob=new aug2024Sel02();
+			ob.method1();
+		}
+	}
